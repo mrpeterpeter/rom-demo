d2397fb8d621f8144736c7fa62acdd4d9d968b13
Piotr Solnica <piotr.solnica@gmail.com>
2014-12-29 18:23:27 +0100

    Remove yaml example and add a second github example

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem 'rom', github: 'rom-rb/rom', branch: 'master'
 gem 'rom-sql', github: 'rom-rb/rom-sql', branch: 'master'
+gem 'rom-yaml', github: 'rom-rb/rom-yaml', branch: 'master'
 
 gem 'faraday'
 gem 'sqlite3'
---
+++ examples/github_two.rb
@@ -0,0 +1,56 @@
+require 'rom-yaml'
+require './lib/github_adapter'
+
+setup = ROM.setup(github: 'github://repos/rom-rb/rom')
+
+setup.schema do
+  base_relation :commits do
+    repository :github
+
+    attribute 'sha'
+    attribute 'commit'
+  end
+end
+
+setup.relation(:commits) do
+  def recent
+    project('sha', 'commit', 'author').take(10)
+  end
+end
+
+setup.mappers do
+
+  define :commits, symbolize_keys: true do
+    model name: 'Commit'
+
+    attribute :sha
+
+    embedded :details, from: 'commit', type: Hash do
+      model name: 'Details'
+      attribute :message
+
+      embedded :author, type: Hash do
+        model name: 'DetailsAuthor'
+        attribute :name
+        attribute :email
+        attribute :timestamp, from: 'date', type: :datetime
+      end
+    end
+
+    embedded :author, type: Hash do
+      model name: 'CommitAuthor'
+      attribute :login
+    end
+  end
+
+end
+
+rom = setup.finalize
+
+commits = rom.read(:commits).recent
+
+commits.each do |commit|
+  puts "sha: #{commit.sha}"
+  puts "  author: #{commit.author.login}"
+  puts "  time: #{commit.details.author.timestamp}"
+end
